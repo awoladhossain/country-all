56995691a219bd9963f50bb73bc0779851a6acdb
Awolad Hossain <awoladh04@gmail.com>
2023-09-08 23:31:04 +0600

    search filed

--- src/components/Country/LoadCountry.jsx
+++ src/components/Country/LoadCountry.jsx
@@ -7,9 +7,7 @@ import Country from './Country';
 const LoadCountry = () => {
 
     const [data, setData] = useState([])
-    const [serachItem, setSearchItem] = useState('')
     
-
     useEffect(() => {
         // const loadData = async () => {
         //     const res = await axios.get('https://restcountries.com/v3.1/all')
@@ -26,20 +24,25 @@ const LoadCountry = () => {
             loadData()
     },[])
 
-
+    const handleCountry = (name) => {
+        console.log(name);
+        fetch(`https://restcountries.com/v3.1/name/${name}`)
+            .then((res) => res.json())
+            .then((data) => setData(data) )
+    }
 
     return (
         <div>
-            <div>
-                <input type="text" placeholder="Type here" className="input input-bordered input-error w-full max-w-xs" onChange={(event) => setSearchItem(event.target.value)} />
+            <h1 className='text-3xl font-bold text-center mb-6'>All Country</h1>
+            <div className='text-center'>
+                <input type="text" placeholder="Search Country" className="input input-bordered input-error w-full max-w-xs" onChange={(event) => handleCountry(event.target.value)} />
            </div>
             <div className="grid grid-cols-3 mt-10">
-                <h1>this is load Country Api Section</h1>
+                {/* <h1>this is load Country Api Section</h1> */}
                 {
-                    data.map((singleCountry, index) => <Country key={index} singleCountry={singleCountry} ></Country>)
+                 data.map((singleCountry, index) => <Country key={index} singleCountry={singleCountry} ></Country>)
                     
                 }
-               
             </div>
        </div>
     );
